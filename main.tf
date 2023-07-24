9f379d6880b89b28c06fd3a31755364226aff686
Tomas Narbutaitis <tomasnar@gmail.com>
2023-07-24 22:00:34 +0800

    change module

--- main.tf
+++ main.tf
@@ -3,8 +3,14 @@ locals {
   region = "us-east1"
 }
 
-module "mig" {
-  source = "./modules/mig"
+# module "mig" {
+#   source = "./modules/mig"
+#   project_id = local.project_id
+#   region = local.region
+# }
+
+module "k8s" {
+  source = "./modules/k8_cluster"
   project_id = local.project_id
   region = local.region
-}
+ }
